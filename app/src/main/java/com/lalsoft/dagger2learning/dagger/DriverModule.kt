d7de0220632b9c1bae987cd13a3b121aea56345d
Shijilal <shijilal@hotmail.com>
2020-09-10 18:18:30 +0530

    Converted Wheels and Driver Module to Object

--- app/src/main/java/com/lalsoft/dagger2learning/dagger/DriverModule.kt
+++ app/src/main/java/com/lalsoft/dagger2learning/dagger/DriverModule.kt
@@ -6,7 +6,7 @@ import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-class DriverModule {
+object DriverModule {
     @Provides
     @Singleton
     fun provideDriver(): Driver {
--- app/src/main/java/com/lalsoft/dagger2learning/dagger/WheelsModule.kt
+++ app/src/main/java/com/lalsoft/dagger2learning/dagger/WheelsModule.kt
@@ -7,7 +7,7 @@ import dagger.Module
 import dagger.Provides
 
 @Module
- class WheelsModule {
+ object WheelsModule {
     @Provides
     fun provideRims(): Rims {
         return Rims()
